f7238e98f6ec46fef8b15ec0ee49928dc23ebbb4
subeen1007 <cc27c@naver.com>
2021-08-28 15:21:10 +0900

    [싱글톤패턴] Thread시 오류

---
+++ src/main/java/Singleton/case1/Database.java
@@ -0,0 +1,28 @@
+package Singleton.case1;
+
+public class Database {
+    private static Database singleton;
+    private String name;
+
+    private Database(String name) {
+//        super();
+//        this.name = name;
+        try{
+            Thread.sleep(100);
+            this.name = name;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Database getInstance(String name) {
+        if(singleton == null){
+            singleton = new Database(name);
+        }
+        return singleton;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/main/java/Singleton/case1/TestPattern1.java
@@ -0,0 +1,13 @@
+package Singleton.case1;
+
+public class TestPattern1 {
+    public static void main(String[] args) {
+        Database database;
+
+        database = Database.getInstance("첫번째 데베");
+        System.out.println("This is " + database.getName());
+
+        database = Database.getInstance("두번 데베");
+        System.out.println("This is " + database.getName());
+    }
+}
